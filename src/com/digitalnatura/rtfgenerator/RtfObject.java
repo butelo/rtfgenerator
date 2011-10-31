eea1948e0d5138b88017439099ebb1d0be0949eb
iria <iria@192.168.1.105>
2011-10-31 20:53:08 +0100

    y

---
+++ src/com/digitalnatura/rtfgenerator/RtfObject.java
@@ -0,0 +1,161 @@
+package com.digitalnatura.rtfgenerator;
+
+public class RtfObject {
+	public String cabecera1;
+	public String cabecera2;
+	public String cabecera3;
+	public String cabecera4;
+	public String cabecera5;
+	public String cabecera6;
+	public String cabecera7;
+	public String cabecera8;
+	public String cabecera9;
+	public String cabecera10;
+	public String cabecera11;
+	
+	
+	
+	public String getCabecera1() {
+		return cabecera1;
+	}
+
+
+
+	public void setCabecera1(String cabecera1) {
+		this.cabecera1 = cabecera1;
+	}
+
+
+
+	public String getCabecera2() {
+		return cabecera2;
+	}
+
+
+
+	public void setCabecera2(String cabecera2) {
+		this.cabecera2 = cabecera2;
+	}
+
+
+
+	public String getCabecera3() {
+		return cabecera3;
+	}
+
+
+
+	public void setCabecera3(String cabecera3) {
+		this.cabecera3 = cabecera3;
+	}
+
+
+
+	public String getCabecera4() {
+		return cabecera4;
+	}
+
+
+
+	public void setCabecera4(String cabecera4) {
+		this.cabecera4 = cabecera4;
+	}
+
+
+
+	public String getCabecera5() {
+		return cabecera5;
+	}
+
+
+
+	public void setCabecera5(String cabecera5) {
+		this.cabecera5 = cabecera5;
+	}
+
+
+
+	public String getCabecera6() {
+		return cabecera6;
+	}
+
+
+
+	public void setCabecera6(String cabecera6) {
+		this.cabecera6 = cabecera6;
+	}
+
+
+
+	public String getCabecera7() {
+		return cabecera7;
+	}
+
+
+
+	public void setCabecera7(String cabecera7) {
+		this.cabecera7 = cabecera7;
+	}
+
+
+
+	public String getCabecera8() {
+		return cabecera8;
+	}
+
+
+
+	public void setCabecera8(String cabecera8) {
+		this.cabecera8 = cabecera8;
+	}
+
+
+
+	public String getCabecera9() {
+		return cabecera9;
+	}
+
+
+
+	public void setCabecera9(String cabecera9) {
+		this.cabecera9 = cabecera9;
+	}
+
+
+
+	public String getCabecera10() {
+		return cabecera10;
+	}
+
+
+
+	public void setCabecera10(String cabecera10) {
+		this.cabecera10 = cabecera10;
+	}
+
+
+
+	public String getCabecera11() {
+		return cabecera11;
+	}
+
+
+
+	public void setCabecera11(String cabecera11) {
+		this.cabecera11 = cabecera11;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return String
+				.format("RtfObject [cabecera1=%s, cabecera2=%s, cabecera3=%s, cabecera4=%s, cabecera5=%s, cabecera6=%s, cabecera7=%s, cabecera8=%s, cabecera9=%s, cabecera10=%s, cabecera11=%s]",
+						cabecera1, cabecera2, cabecera3, cabecera4, cabecera5,
+						cabecera6, cabecera7, cabecera8, cabecera9, cabecera10,
+						cabecera11);
+	}
+
+
+
+}
---
+++ src/com/digitalnatura/rtfgenerator/RtfgeneratorActivity.java
@@ -0,0 +1,71 @@
+package com.digitalnatura.rtfgenerator;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+
+public class RtfgeneratorActivity extends Activity {
+
+	String titulo = "TITULO MAYÚSCULA";
+	String by = "by";
+	String nome = "Xes García Santamarina";
+	String direccion = "C/ Álvaro Cunqueiro nº2 8ºB";
+	String cp = "A Coruña CP. 15008 Spain";
+	String tf = "Tf. +34645657166";
+	String email = "email: xesgarcia@gmail.com";
+
+	String encabezamento = "EXT." + "FOREST" + "DAY";
+	String fadein = "FADE IN" + ":";
+	String descripcion = "DOROTHY, TIN MAN, SCARECROW and TOTO walk through a thick forest in the Land of Oz. Dorothy carries a basket, the Tin Man carries an axe and an oil can. The á é í ó ú Á É Í Ó Ú ñ Ñ ç Ç ü Ü ¿ ¡  ride branches and dead leaves. The Emerald City is seen far in the distance.";
+	String personaje = "DOROTHY";
+	String acotacion = "(perfecto xa esta)";
+	String dialogo = "I wonder what we will do for the forest and kill a deer for you. You can roast it by the fire, since your tastes are so peculiar.";
+	
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		cargarTemplate();
+		
+		generarString();
+		
+
+		try {
+			escribirString();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+
+	private void escribirString() throws IOException {
+		// TODO Auto-generated method stub
+		 File newTextFile = new File(Environment.getExternalStorageDirectory()+"/"+"json.json");
+	     FileWriter fw = new FileWriter(newTextFile);
+	     fw.write(dialogo);
+	     fw.close();
+		
+	}
+
+
+	private void generarString() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private void cargarTemplate() {
+		// TODO Auto-generated method stub
+		
+	}
+}
